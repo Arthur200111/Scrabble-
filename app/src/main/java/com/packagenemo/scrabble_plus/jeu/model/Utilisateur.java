8e34011a00a2394fccb71ee0de3d31c0d108dc8f
CanalThomas <tomacanal@hotmail.fr>
2023-03-03 17:20:00 +0100

    Ajout des packages repositories et manager

---
+++ app/src/main/java/com/packagenemo/scrabble_plus/jeu/model/Utilisateur.java
@@ -0,0 +1,13 @@
+package com.packagenemo.scrabble_plus.jeu.model;
+
+import java.util.List;
+
+public class Utilisateur {
+    private String uid;
+    private String nom;
+
+    private List<Partie> PartiesJouees;
+
+    private List<Partie> PartiesEnCours;
+
+}
