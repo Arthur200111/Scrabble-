6e4bb016ba8d0227550b9dd60883fb316775184e
Arthur200111 <arthur.huet01@gmail.com>
2023-03-21 22:50:05 +0100

    TOODOs resumeActivity

--- app/src/main/java/com/packagenemo/scrabble_plus/resume/ResumeActivity.java
+++ app/src/main/java/com/packagenemo/scrabble_plus/resume/ResumeActivity.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.packagenemo.scrabble_plus.R;
+import com.packagenemo.scrabble_plus.jeu.ui.JeuActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.LinkedList;
@@ -24,18 +27,27 @@ public class ResumeActivity extends AppCompatActivity {
         // Initialisation du menu déroulant contenant toutes les parties en cours
         mResumeRecyclerView = findViewById(R.id.resumeRecyclerView);
 
-        adapter = new PartyAdapter(new LinkedList<>());
+        adapter = new PartyAdapter(new LinkedList<>(), this);
         mResumeRecyclerView.setHasFixedSize(true);
         mResumeRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mResumeRecyclerView.setAdapter(adapter);
 
         // Implémentation d'exemples de joueur
-        addNewParty("jeu 1", "à toi de jouer", "");
-        addNewParty("jeu 2", "tu joues dans 2min", "");
-        addNewParty("jeu 3", "à Tristan de jouer", "");
-        addNewParty("jeu 4", "bientôt à toi", "");
-        addNewParty("jeu 5", "en attente de Thomas ...", "");
-        addNewParty("jeu 6", "Il te reste 15min", "");
+        // TODO Ici à la place d'une implémentation brut de parties il faut les charger
+        // Il faudrait donc pouvoir avoir le titre et savoir si c'est à l'utilisateur de jouer ou non
+        addNewParty("XVGTEDFFF","jeu 1", "à toi de jouer", "");
+        addNewParty("HUFOZLKFF", "jeu 2", "tu joues dans 2min", "");
+        addNewParty("FEQF4EFFF", "jeu 3", "à Tristan de jouer", "");
+        addNewParty("KFJZ7ZFJI", "jeu 4", "bientôt à toi", "");
+        addNewParty("ZFFFE5ZDZ", "jeu 5", "en attente de Thomas ...", "");
+        addNewParty("ZDFZF5ZFZ", "jeu 6", "Il te reste 15min", "");
+    }
+
+    public void redirectToPArty(String id) {
+        // TODO fonction appelé lorsque l'on clique sur l'une des parties pour aller sur la gameActivity
+        Intent toPageGameIntent = new Intent(ResumeActivity.this, JeuActivity.class);
+        toPageGameIntent.putExtra("partyId",id);
+        startActivity(toPageGameIntent);
     }
 
     /**
@@ -45,8 +57,8 @@ public class ResumeActivity extends AppCompatActivity {
      * @param partyState état de la partie
      * @param partyImgLabel nom de l'image utilisé pour la partie
      */
-    public void addNewParty(String partyName, String partyState, String partyImgLabel) {
-        adapter.update(new PartyData(partyName,partyState,getPartyImgFromLabel(partyImgLabel)));
+    public void addNewParty(String id, String partyName, String partyState, String partyImgLabel) {
+        adapter.update(new PartyData(partyName,partyState,getPartyImgFromLabel(partyImgLabel), id));
     }
 
     /**
